a82a2c3ea84823e7d57dbc257f41d150d50c01bd
Xu An <109224240+XuAn-universe@users.noreply.github.com>
2025-07-25 16:13:59 -0400

    Update FFT_trajectory.m

--- FFT_trajectory.m
+++ FFT_trajectory.m
@@ -12,9 +12,6 @@ f = (0:NumUniquePts-1)*2*Fn/NFFT;
 signal = (trajectory-mean(trajectory)).*winfunc;
 FFTX = fft(signal, NFFT);               % Take FFT, padding with zeros. length(FFTX)==NFFT
 FFTX = FFTX(1:NumUniquePts);            % FFT is symmetric, throw away second half
-MX = abs(FFTX);                         % Take magnitude of X, also equal to sqrt(FFTX.*conj(FFTX))
-MX = MX*2;                              % Multiply by 2 to take into account the fact that we threw out second half of FFTX above
-MX(1) = MX(1)/2;                        % Account for endpoint uniqueness
-MX(length(MX)) = MX(length(MX))/2;      % We know NFFT is even
-MX = MX/length(signal);                 % Scale the FFT so that it is not a function of the length of x.
-MX = MX.^2;
+MX = abs(FFTX).^2;                       
+MX(2:end-1) = 2*MX(2:end-1);
+MX = MX/sum(winfunc.^2);
